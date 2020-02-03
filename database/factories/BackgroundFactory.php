337a7b6df90cbc6bd05641f19d6d3bcb7d576d97
Abdulaziz-al <abdoal0aziz133@gmail.com>
2020-02-03 14:44:25 +0300

    create_tables

---
+++ database/factories/BackgroundFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Background;
+use Faker\Generator as Faker;
+
+$factory->define(Background::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
