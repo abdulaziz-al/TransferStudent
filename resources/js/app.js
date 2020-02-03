26f5aca72601414ebe6d48e2291c3170cd7104d3
Abdulaziz-al <abdoal0aziz133@gmail.com>
2020-02-03 12:04:40 +0300

    vue_composer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+
+
+
+window.Vue = require('vue');
+
+Vue.component('test',require('./components/Test.vue').default);
+
+Vue.component('navbar',require('./components/navbar.vue').default);
+
+
+const app = new Vue({
+    el: '#app'
+});
